fa9ed5bf10c643e8e26786d0ddb7b9e68aee5599
Aniko-S <s.aniko9424@gmail.com>
2021-01-23 12:20:46 +0100

    Created a spring boot application

---
+++ spring-server/hollandkocsma/src/main/java/com/github/anikos/hollandkocsma/GameApplication.java
@@ -0,0 +1,13 @@
+package com.github.anikos.hollandkocsma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
---
+++ spring-server/hollandkocsma/src/test/java/com/github/anikos/hollandkocsma/HollandkocsmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.anikos.hollandkocsma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollandkocsmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
