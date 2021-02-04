a30aef6d7d4555b69eae7542c15791ae73ad5e14
Aniko-S <s.aniko9424@gmail.com>
2021-02-04 20:56:18 +0100

    Shown cards not moving on the table.

--- react-client/hollandkocsma/src/cardLists/BlindAndShownCardList.js
+++ react-client/hollandkocsma/src/cardLists/BlindAndShownCardList.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BlindAndShownCard from '../cardFiles/BlindAndShownCard';
 
 function BlindAndShownCardList({ listShown, setIds, ids, putFromBlind, blindAvailable }) {
 
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const difference = listShown.filter(id => !cards.includes(id));
+    if (difference.length !== 1) {
+      setCards(listShown);
+      return;
+    }
+    console.log('listShown: ', listShown);
+    console.log('cards: ', cards);
+
+    const cardsNotContainsId = difference[0];
+    console.log('cardsNotContainsId: ', cardsNotContainsId);
+   // const listShownNotContainsIndex = cards.findIndex(() => cards.filter(id => !listShown.includes(id))[0]);
+
+    const listShownNotContainsIndex = cards.findIndex(id => !listShown.includes(id));
+    console.log('listShownNotContainsIndex: ', listShownNotContainsIndex);
+    const newCards = [...cards];
+    newCards[listShownNotContainsIndex] = cardsNotContainsId;
+    setCards(newCards);
+  }, [listShown]);
+
   return (
     <div className='cardList'>
-      {listShown.map(id => 
+      {cards.map(id => 
         <BlindAndShownCard key={id} 
           shownId={id} 
           setIds={setIds} 
