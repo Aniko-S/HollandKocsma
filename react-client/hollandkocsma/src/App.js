52dbfaba9c5ead2b62a4abd65e3d34c99b00ade8
Aniko-S <s.aniko9424@gmail.com>
2021-03-08 20:08:05 +0100

    Add basename to Router

--- react-client/hollandkocsma/src/App.js
+++ react-client/hollandkocsma/src/App.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import GameBoard from './menuElements/GameBoard';
-import Start from './menuElements/Start';
-import './style.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import GameBoard from "./menuElements/GameBoard";
+import Start from "./menuElements/Start";
+import "./style.css";
 
 function App() {
   const dataArray = useState();
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route path='/game'>
+        <Route path="/game">
           <GameBoard dataArray={dataArray} />
         </Route>
-        <Route path=''>
+        <Route path="">
           <Start dataArray={dataArray} />
         </Route>
       </Switch>
