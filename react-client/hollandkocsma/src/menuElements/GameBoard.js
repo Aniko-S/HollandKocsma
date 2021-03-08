0f281a869e323cc1ff28370b25b94a28762b481d
Aniko-S <s.aniko9424@gmail.com>
2021-03-08 14:35:47 +0100

    Delete unused useEffect and gameData

--- react-client/hollandkocsma/src/menuElements/GameBoard.js
+++ react-client/hollandkocsma/src/menuElements/GameBoard.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Machine from '../mainPartitions/Machine';
-import Player from '../mainPartitions/Player';
-import Table from '../mainPartitions/Table';
-import PopUp from '../PopUp';
+import React, { useState } from "react";
+import axios from "axios";
+import Machine from "../mainPartitions/Machine";
+import Player from "../mainPartitions/Player";
+import Table from "../mainPartitions/Table";
+import PopUp from "../PopUp";
 
 function GameBoard({ dataArray }) {
   const [gameData, setGameData] = dataArray;
@@ -18,11 +18,11 @@ function GameBoard({ dataArray }) {
       }
     }
     return false;
-  }
+  };
 
   const deleteId = (id) => {
-    setSelectedCardsIds(ids => ids.filter(actId => actId !== id));
-  }
+    setSelectedCardsIds((ids) => ids.filter((actId) => actId !== id));
+  };
 
   const setIds = (id) => {
     if (contains(id)) {
@@ -30,16 +30,22 @@ function GameBoard({ dataArray }) {
     } else {
       setSelectedCardsIds([...selectedCardsIds, id]);
     }
-  }
+  };
 
   async function playerPutCardsToShown() {
-    const { data } = await axios.post(`http://localhost:8080/game/toshown`, selectedCardsIds);
+    const { data } = await axios.post(
+      `http://localhost:8080/game/toshown`,
+      selectedCardsIds
+    );
     setGameData(data);
     setTheButtonsFunction(data);
   }
 
   async function playerPutCardsToPile() {
-    const { data } = await axios.post(`http://localhost:8080/game/game`, selectedCardsIds);
+    const { data } = await axios.post(
+      `http://localhost:8080/game/game`,
+      selectedCardsIds
+    );
     setGameData(data);
     setSelectedCardsIds([]);
     isMachinesTurn(data);
@@ -75,49 +81,62 @@ function GameBoard({ dataArray }) {
     if (data?.isTurnFinished && !data?.playersData?.isWinner) {
       machinePutCardsToPile();
     }
-  }
+  };
 
   const isMachinesTurnFinished = (data) => {
     if (!data?.isTurnFinished) {
       machinePutCardsToPile();
     }
-  }
+  };
 
   return (
-    <div className='board'>
-      {gameData?.machinesData?.isWinner 
-        && <PopUp title='Sorry, Bob is the winner' 
-              body='Play again, maybe you will have more luck in the next game.' />}
-      {gameData?.playersData?.isWinner 
-        && <PopUp title='Congratulations, you are the winner' 
-              body='Play again to test your luck' />}
-      <div className='playerSpace'>
-        {gameData?.machinesData 
-          && <Machine hand={gameData.machinesData.handCardsNumber} 
-                listShown={gameData.machinesData.shownCardsIds} 
-                blindNumber={gameData.machinesData.blindCardsNumber} />}
+    <div className="board">
+      {gameData?.machinesData?.isWinner && (
+        <PopUp
+          title="Sorry, Bob is the winner"
+          body="Play again, maybe you will have more luck in the next game."
+        />
+      )}
+      {gameData?.playersData?.isWinner && (
+        <PopUp
+          title="Congratulations, you are the winner"
+          body="Play again to test your luck"
+        />
+      )}
+      <div className="playerSpace">
+        {gameData?.machinesData && (
+          <Machine
+            hand={gameData.machinesData.handCardsNumber}
+            listShown={gameData.machinesData.shownCardsIds}
+            blindNumber={gameData.machinesData.blindCardsNumber}
+          />
+        )}
       </div>
-      <div className='tableSpace'>
-        {gameData?.tablesData 
-          && <Table deck={gameData.tablesData.hasDeck} 
-                pile={gameData.tablesData.pileTop} 
-                message={gameData.tablesData.message} 
-                setIds={setIds} 
-                pickUpThePile={playerPickUpThePile}      
-              />
-        }
+      <div className="tableSpace">
+        {gameData?.tablesData && (
+          <Table
+            deck={gameData.tablesData.hasDeck}
+            pile={gameData.tablesData.pileTop}
+            message={gameData.tablesData.message}
+            setIds={setIds}
+            pickUpThePile={playerPickUpThePile}
+          />
+        )}
       </div>
-      <div className='playerSpace'>
-        {gameData?.playersData 
-          && <Player name={gameData.playersData.name} 
-                listHand={gameData.playersData.handCardsIds} 
-                listShown={gameData.playersData.shownCardsIds} 
-                setIds={setIds} 
-                putCards={filledShown ? playerPutCardsToPile : playerPutCardsToShown} 
-                ids={selectedCardsIds} 
-                putFromBlind={playerPutFromBlind}
-              />
-        }
+      <div className="playerSpace">
+        {gameData?.playersData && (
+          <Player
+            name={gameData.playersData.name}
+            listHand={gameData.playersData.handCardsIds}
+            listShown={gameData.playersData.shownCardsIds}
+            setIds={setIds}
+            putCards={
+              filledShown ? playerPutCardsToPile : playerPutCardsToShown
+            }
+            ids={selectedCardsIds}
+            putFromBlind={playerPutFromBlind}
+          />
+        )}
       </div>
     </div>
   );
--- react-client/hollandkocsma/src/menuElements/Start.js
+++ react-client/hollandkocsma/src/menuElements/Start.js
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Start({ dataArray }) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   async function newGame() {
-    const name = text || 'Player';  
-    const [gameData, setGameData] = dataArray;
-    const { data } = await axios.post(`http://localhost:8080/game/begin/${name}`, name);
+    const name = text || "Player";
+    const setGameData = dataArray[1];
+    const { data } = await axios.post(
+      `http://localhost:8080/game/begin/${name}`,
+      name
+    );
     setGameData(data);
   }
 
   return (
-    <div className='start'>
-      <div className='nametext'>Welcome in the game. You play against Bob.</div>
-      <div className='nametext'>Enter your name and start the game.</div>
-      <div className='nametext'>Good luck!</div>
-      
-      <input className='input' 
-        type='text' value={text} 
-        placeholder='Your name' 
-        onChange={e => setText(e.target.value)} 
+    <div className="start">
+      <div className="nametext">Welcome in the game. You play against Bob.</div>
+      <div className="nametext">Enter your name and start the game.</div>
+      <div className="nametext">Good luck!</div>
+
+      <input
+        className="input"
+        type="text"
+        value={text}
+        placeholder="Your name"
+        onChange={(e) => setText(e.target.value)}
       />
-      <Link to='/game'>
-        <button className='button name' onClick={newGame}>Start the game</button>
+      <Link to="/game">
+        <button className="button name" onClick={newGame}>
+          Start the game
+        </button>
       </Link>
     </div>
   );
