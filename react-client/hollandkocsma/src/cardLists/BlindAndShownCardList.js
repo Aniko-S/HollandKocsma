e2449b989f9420786101b718ffa3d5ff16933d38
Aniko-S <s.aniko9424@gmail.com>
2021-03-08 14:36:43 +0100

    Add dependency to useEffect

--- react-client/hollandkocsma/src/cardLists/BlindAndShownCardList.js
+++ react-client/hollandkocsma/src/cardLists/BlindAndShownCardList.js
@@ -1,34 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import BlindAndShownCard from '../cardFiles/BlindAndShownCard';
+import React, { useState, useEffect } from "react";
+import BlindAndShownCard from "../cardFiles/BlindAndShownCard";
 
-function BlindAndShownCardList({ listShown, setIds, ids, putFromBlind, blindAvailable, shownAvailable }) {
-
+function BlindAndShownCardList({
+  listShown,
+  setIds,
+  ids,
+  putFromBlind,
+  blindAvailable,
+  shownAvailable,
+}) {
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    const difference = listShown.filter(id => !cards.includes(id));
+    const difference = listShown.filter((id) => !cards.includes(id));
     if (difference.length !== 1) {
       setCards(listShown);
       return;
     }
     const cardsNotContainsId = difference[0];
-    const listShownNotContainsIndex = cards.findIndex(id => !listShown.includes(id));
+    const listShownNotContainsIndex = cards.findIndex(
+      (id) => !listShown.includes(id)
+    );
     const newCards = [...cards];
     newCards[listShownNotContainsIndex] = cardsNotContainsId;
     setCards(newCards);
-  }, [listShown]);
+  }, [listShown, cards]);
 
   return (
-    <div className='cardList'>
-      {cards.map(id => 
-        <BlindAndShownCard key={id} 
-          shownId={id} 
-          setIds={setIds} 
-          ids={ids} 
-          putFromBlind={putFromBlind} 
+    <div className="cardList">
+      {cards.map((id) => (
+        <BlindAndShownCard
+          key={id}
+          shownId={id}
+          setIds={setIds}
+          ids={ids}
+          putFromBlind={putFromBlind}
           blindAvailable={blindAvailable}
           shownAvailable={shownAvailable}
-        />)}
+        />
+      ))}
     </div>
   );
 }
