c14d11e374664942cafb25439c18375952794487
Aniko-S <s.aniko9424@gmail.com>
2021-03-09 18:23:12 +0100

    Use isPlayersTurn to manage cards' availability

--- react-client/hollandkocsma/src/mainPartitions/Player.js
+++ react-client/hollandkocsma/src/mainPartitions/Player.js
@@ -1,8 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import CardList from '../cardLists/CardList';
-import BlindAndShownCardList from '../cardLists/BlindAndShownCardList';
+import React, { useState, useEffect } from "react";
+import CardList from "../cardLists/CardList";
+import BlindAndShownCardList from "../cardLists/BlindAndShownCardList";
 
-function Player({ name, listHand, listShown, setIds, putCards, ids, putFromBlind }) {
+function Player({
+  name,
+  listHand,
+  listShown,
+  setIds,
+  putCards,
+  ids,
+  putFromBlind,
+  isPlayersTurn,
+}) {
   const [blindAvailable, setBlindAvailable] = useState(false);
 
   useEffect(() => {
@@ -13,36 +22,41 @@ function Player({ name, listHand, listShown, setIds, putCards, ids, putFromBlind
 
   return (
     <>
-      <div className='playerLine'>
-        <div className='container'>
-          <div className='row align-items-center'>
-            <div className='col-3'></div>
-            <div className='col-6'>
-              <BlindAndShownCardList listShown={listShown}
-                setIds={setIds} ids={ids}
+      <div className="playerLine">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-3"></div>
+            <div className="col-6">
+              <BlindAndShownCardList
+                listShown={listShown}
+                setIds={setIds}
+                ids={ids}
                 putFromBlind={putFromBlind}
-                blindAvailable={blindAvailable}
-                shownAvailable={listHand.length === 0}
+                blindAvailable={isPlayersTurn && blindAvailable}
+                shownAvailable={isPlayersTurn && listHand.length === 0}
               />
             </div>
-            <div className='col-3'>
-              <button className='button put' onClick={putCards}>Put</button>
+            <div className="col-3">
+              <button className="button put" onClick={putCards}>
+                Put
+              </button>
             </div>
           </div>
         </div>
       </div>
-      <div className='playerLine'>
-        <div className={listHand.length > 25 ? 'cardList low' : 'cardList'}>
-          <CardList className='hand'
+      <div className="playerLine">
+        <div className={listHand.length > 25 ? "cardList low" : "cardList"}>
+          <CardList
+            className="hand"
             cardIdList={listHand}
             setIds={setIds}
             ids={ids}
             isCover={true}
-            available={true}
+            available={isPlayersTurn}
           />
         </div>
       </div>
-      <div className='name'>{name}</div>
+      <div className="name">{name}</div>
     </>
   );
 }
