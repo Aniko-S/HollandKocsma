df71f2030c927c47240dbf96230547e8a1bc3c6d
Aniko-S <s.aniko9424@gmail.com>
2021-03-11 10:02:59 +0100

    Delete unused useState and useEffect

--- react-client/hollandkocsma/src/mainPartitions/Player.js
+++ react-client/hollandkocsma/src/mainPartitions/Player.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CardList from "../cardLists/CardList";
 import BlindAndShownCardList from "../cardLists/BlindAndShownCardList";
 
